47a59786530ca0c28e7a6557481391485e246e8e
Joy Barot <joybarot720@gmail.com>
2025-02-27 21:34:40 +0530

    modified:  update-readme.js

--- update-readme.js
+++ update-readme.js
@@ -1,41 +1,65 @@
-const fs = require('fs');
-const https = require('https');
+const fs = require("fs");
+const https = require("https");
 
 const username = process.env.USERNAME;
+const excludedRepos = ["Exonymos", "repo1", "repo2", "repo5"]; // Repos to exclude
+
 const options = {
-  hostname: 'api.github.com',
+  hostname: "api.github.com",
   path: `/users/${username}/repos?sort=updated&per_page=3`,
-  method: 'GET',
+  method: "GET",
   headers: {
-    'User-Agent': username,
-    'Authorization': `token ${process.env.GITHUB_TOKEN}`,
+    "User-Agent": username,
+    Authorization: `token ${process.env.GITHUB_TOKEN}`,
   },
 };
 
-https.get(options, (res) => {
-  let data = '';
-  res.on('data', chunk => { data += chunk; });
-  res.on('end', () => {
-    const repos = JSON.parse(data);
-    let projectsMarkdown = '';
-    const excludedRepos = ['Exonymos']; // repos to exclude from the list
-    repos.forEach(repo => {
-      projectsMarkdown += `### 🌟 [${repo.name}](${repo.html_url})\n`;
-      projectsMarkdown += `- 🔗 **[GitHub Repo](${repo.html_url})** - ${repo.description || 'No description provided'}\n\n`;
+function fetchRepos(callback) {
+  https
+    .get(options, (res) => {
+      let data = "";
+      res.on("data", (chunk) => {
+        data += chunk;
+      });
+      res.on("end", () => {
+        const repos = JSON.parse(data);
+        callback(repos);
+      });
+    })
+    .on("error", (e) => {
+      console.error(e);
     });
-    
-    const readmePath = './README.md';
-    let readmeContent = fs.readFileSync(readmePath, 'utf8');
+}
 
-    const startMarker = '<!-- LATEST_PROJECTS:START -->';
-    const endMarker = '<!-- LATEST_PROJECTS:END -->';
-    const regex = new RegExp(`${startMarker}[\\s\\S]*${endMarker}`, 'm');
-
-    const newSection = `${startMarker}\n${projectsMarkdown}${endMarker}`;
-    readmeContent = readmeContent.replace(regex, newSection);
-
-    fs.writeFileSync(readmePath, readmeContent, 'utf8');
+function generateMarkdown(repos) {
+  let projectsMarkdown = "";
+  repos.forEach((repo) => {
+    if (excludedRepos.includes(repo.name)) {
+      return;
+    }
+    projectsMarkdown += `### 🌟 [${repo.name}](${repo.html_url})\n`;
+    projectsMarkdown += `- 🔗 **[GitHub Repo](${repo.html_url})** - ${
+      repo.description || "No description provided"
+    }\n\n`;
   });
-}).on('error', (e) => {
-  console.error(e);
+  return projectsMarkdown;
+}
+
+function updateReadme(markdown) {
+  const readmePath = "./README.md";
+  let readmeContent = fs.readFileSync(readmePath, "utf8");
+
+  const startMarker = "<!-- LATEST_PROJECTS:START -->";
+  const endMarker = "<!-- LATEST_PROJECTS:END -->";
+  const regex = new RegExp(`${startMarker}[\\s\\S]*${endMarker}`, "m");
+
+  const newSection = `${startMarker}\n${markdown}${endMarker}`;
+  readmeContent = readmeContent.replace(regex, newSection);
+
+  fs.writeFileSync(readmePath, readmeContent, "utf8");
+}
+
+fetchRepos((repos) => {
+  const markdown = generateMarkdown(repos);
+  updateReadme(markdown);
 });
